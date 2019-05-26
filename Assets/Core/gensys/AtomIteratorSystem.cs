bd25c979ef7f94fda1419b882d5f0c664a642434
Elliot Bentine <ebentine@bitbucket.org>
2019-05-26 17:04:29 +0100

    Started work to replace harmonic trap where I could with something more generic, and that ideally would also work for the RF evap (hard, b/c it requires entity buffer) and also for measuring temperatures, potential energies, etc (hard because they require the 'effector' to change). I'm not really convinced this has helped, and unfortunately can't spend more time on it today. Might be worth waiting to see how the API changes over time, because I don't think it is ready just yet.

---
+++ Assets/Core/gensys/AtomIteratorSystem.cs
@@ -0,0 +1,121 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+/// <summary>
+/// A generic system that updates entities (effectors) that enumerate over atoms
+/// </summary>
+public abstract class AtomIteratorSystem<TEffector, TAtomComponent, TProcess> : JobComponentSystem
+    where TEffector : struct, IComponentData
+    where TProcess : struct, IAtomIteratorStrategy<TEffector, TAtomComponent>
+    where TAtomComponent : struct, IComponentData
+{
+    /// <summary>
+    /// An entity query which selects all harmonic trap entities.
+    /// </summary>
+    protected EntityQuery AtomIteratorQuery;
+
+    /// <summary>
+    /// An entity query which selects all trapped atoms.
+    /// </summary>
+    protected EntityQuery AtomQuery;
+
+    struct Effector
+    {
+        public TEffector Effect;
+        public Translation Position;
+    }
+    NativeArray<Effector> Effectors;
+
+    protected override JobHandle OnUpdate(JobHandle inputDependencies)
+    {
+        int trapCount = AtomIteratorQuery.CalculateLength();
+        Effectors = new NativeArray<Effector>(trapCount, Allocator.TempJob, NativeArrayOptions.UninitializedMemory);
+
+        var getEffectorsJH = new GetEffectorsJob
+        {
+            Effectors = Effectors,
+        }.Schedule(AtomIteratorQuery, inputDependencies);
+
+        var enumerateAtomsJH = new EnumerateAtomsJob
+        {
+            Effectors = Effectors,
+            AtomPositions = GetArchetypeChunkComponentType<Translation>(true),
+            AtomComponents = GetArchetypeChunkComponentType<TAtomComponent>(false),
+            IteratorStrategy = CreateIteratorStrategy()
+        }.Schedule(AtomQuery, getEffectorsJH);
+
+        return enumerateAtomsJH;
+    }
+
+    [BurstCompile]
+    struct GetEffectorsJob : IJobForEachWithEntity<Translation, TEffector>
+    {
+        public NativeArray<Effector> Effectors;
+
+        public void Execute(Entity e, int i,
+            [ReadOnly] ref Translation translation, [ReadOnly] ref TEffector effect)
+        {
+            Effectors[i] = new Effector
+            {
+                Effect = effect,
+                Position = translation
+            };
+        }
+    }
+
+    /// <summary>
+    /// Calculates the forces applied to each atom by each harmonic trap.
+    /// </summary>
+    [BurstCompile]
+    struct EnumerateAtomsJob : IJobChunk
+    {
+        [DeallocateOnJobCompletion] [ReadOnly] public NativeArray<Effector> Effectors;
+        [ReadOnly] public ArchetypeChunkComponentType<Translation> AtomPositions;
+        public ArchetypeChunkComponentType<TAtomComponent> AtomComponents;
+        public TProcess IteratorStrategy;
+
+        public void Execute(ArchetypeChunk chunk, int chunkIndex, int firstEntityIndex)
+        {
+            var atomPos = chunk.GetNativeArray(AtomPositions);
+            var atomComps = chunk.GetNativeArray(AtomComponents);
+
+            for (int atomId = 0; atomId < atomComps.Length; atomId++)
+            {
+                var atomComponent = atomComps[atomId];
+                for (int trapId = 0; trapId < Effectors.Length; trapId++)
+                {
+                    //IteratorStrategy.Execute(Effectors[trapId], ref atomComponent);
+                }
+                atomComps[atomId] = atomComponent;
+            }
+        }
+    }
+
+    protected abstract TProcess CreateIteratorStrategy();
+
+    protected override void OnCreateManager()
+    {
+        AtomQuery = GetEntityQuery(new EntityQueryDesc
+        {
+            All = new[] {
+                    ComponentType.ReadOnly<Translation>(),
+                    ComponentType.ReadOnly<Force>(),
+                    ComponentType.ReadOnly<Trapped>()
+                }
+        }
+        );
+
+        AtomIteratorQuery = GetEntityQuery(new EntityQueryDesc
+        {
+            All = new[] {
+                    ComponentType.ReadOnly<Translation>(),
+                    ComponentType.ReadOnly<HarmonicTrap>()
+                }
+        }
+        );
+    }
+}
---
+++ Assets/Core/gensys/IAtomIteratorStrategy.cs
@@ -0,0 +1,8 @@
+﻿using Unity.Entities;
+
+public interface IAtomIteratorStrategy<TIterator, TAtomComponent>
+    where TIterator : struct, IComponentData
+    where TAtomComponent : struct, IComponentData
+{
+    void Execute(TIterator iterator, ref TAtomComponent atom);
+}
