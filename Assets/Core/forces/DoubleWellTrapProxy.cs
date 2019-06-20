89845d8cd97147ceed0b8632357751fbc94c13ba
Elliot Bentine <elliot_bentine@hotmail.com>
2019-06-20 16:50:26 +0100

    Added: Double-well trap

---
+++ Assets/Core/forces/DoubleWellTrapProxy.cs
@@ -0,0 +1,18 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class DoubleWellTrapProxy : MonoBehaviour, IConvertGameObjectToEntity
+{
+    [Tooltip("Spring constant of this trap.")]
+    public float SpringConstant = 1f;
+
+    public float Separation = 0f;
+
+    public float Alpha = 1f;
+
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+        dstManager.AddComponentData(entity, new DoubleWellTrap { SpringConstant = SpringConstant, Separation = Separation, alpha = Alpha });
+    }
+}
