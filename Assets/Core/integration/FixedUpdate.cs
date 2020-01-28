074eaf861bc8c1b299134adea236f552a502dba4
Elliot Bentine <ebentine@bitbucket.org>
2020-01-28 21:36:15 +0000

    trying to find cause of integration error

--- Assets/Core/integration/FixedUpdate.cs
+++ Assets/Core/integration/FixedUpdate.cs
@@ -6,7 +6,7 @@ namespace Integration
     [UpdateInGroup(typeof(SimulationSystemGroup))]
     public class FixedUpdateGroup : ComponentSystemGroup
     {
-        public const float FIXED_TIME_DELTA = 1/60.0f;
+        public const float FIXED_TIME_DELTA = 1/120.0f;
 
         private float _TimeSinceLastUpdate = 0f;
 
