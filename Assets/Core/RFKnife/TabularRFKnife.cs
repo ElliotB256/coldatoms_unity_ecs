bc0020a8c11bd2aa38811b2af05b333427bf2c32
Elliot Bentine <elliot_bentine@hotmail.com>
2020-02-16 14:52:57 +0000

    Support for RF knife ramps defined by a table.

---
+++ Assets/Core/RFKnife/TabularRFKnife.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Unity.Entities;
+using Unity.Jobs;
+using Integration;
+
+/// <summary>
+/// Component that signifies RF knife should be controlled via tabular data.
+/// </summary>
+[Serializable]
+[GenerateAuthoringComponent]
+public struct TabularRFKnife : IComponentData
+{
+    public float CurrentTime;
+}
+
+[Serializable]
+[GenerateAuthoringComponent]
+public struct TabularRFKnifeElement : IBufferElementData
+{
+    public float Duration;
+    public float Radius;
+}
+
+/// <summary>
+/// A system that updates the RF knife radius according to a table.
+/// </summary>
+public class TabularRFKnifeSystem : JobComponentSystem
+{
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        float dt = FixedUpdateGroup.FIXED_TIME_DELTA;
+        return Entities.ForEach(
+            (DynamicBuffer<TabularRFKnifeElement> elements, ref TabularRFKnife knife, ref Radius radius)
+                =>
+                {
+                    knife.CurrentTime += dt;
+
+                    // get current table index
+                    int index = 0;
+                    float tableTime = 0f;
+                    float startRadius = elements[0].Radius;
+                    float endRadius = elements[0].Radius;
+                    for (int i = 0; i < elements.Length; i++)
+                    {
+                        index = i;
+                        if (tableTime + elements[i].Duration > knife.CurrentTime)
+                        {
+                            endRadius = elements[i].Radius;
+                            break;
+                        }
+                        else
+                        {
+                            tableTime += elements[i].Duration;
+                            startRadius = elements[i].Radius;
+                        }
+                    }
+
+                    // lerp radius
+                    float nt = (knife.CurrentTime - tableTime)/(elements[index].Duration);
+                    nt = Math.Min(Math.Max(0.0f, nt), 1.0f);
+                    radius.Value = (1 - nt) * startRadius + nt * endRadius;
+                }
+            ).Schedule(inputDeps);
+    }
+}
+
---
+++ Assets/Core/RFKnife/TabularRFKnifeProxy.cs
@@ -0,0 +1,19 @@
+﻿using Unity.Entities;
+using Unity.Rendering;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+[RequiresEntityConversion]
+class TabularRFKnifeProxy : MonoBehaviour, IConvertGameObjectToEntity
+{
+
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+        dstManager.AddBuffer<TabularRFKnifeElement>(entity);
+        dstManager.AddComponentData<TabularRFKnife>(entity, new TabularRFKnife{CurrentTime = 0f});
+        
+        var buffer = dstManager.GetBuffer<TabularRFKnifeElement>(entity);
+        buffer.Add(new TabularRFKnifeElement { Duration = 0.0f, Radius = 10f });
+        buffer.Add(new TabularRFKnifeElement { Duration = 10.0f, Radius = 0f });
+    }
+}
